6c5675912814a334f7222210c9cf5559e4bf9fc0
bassima_gh <ghba20wu@stident.ju.se>
2022-12-29 23:16:01 +0100

    todo app functionality done

---
+++ src/components/TodoList.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import TodoForm from './TodoForm'
+import Todo from './Todo'
+
+function TodoList() {
+	const [todos, setTodos] = useState([]);
+
+	function addTodo (todo) {
+		if (!todo.text || /^\s*$/.test(todo.text)) {
+			return;
+		}
+
+		const newTodos = [todo, ...todos];
+
+		setTodos(newTodos);
+	}
+
+	function updateTodo(todoId, newValue) {
+		if (!newValue.text || /^\s*$/.test(newValue.text)) {
+			return;
+		}
+
+		setTodos(previousTodo => previousTodo.map(item => (item.id === todoId ? newValue : item)))
+	}
+
+	function removeTodo(id) {
+		const removeArr = [...todos].filter(todo => todo.id !== id);
+
+		setTodos(removeArr);
+	}
+
+	function completeTodo(id) {
+		let updateTodos = todos.map(todo => {
+			if (todo.id === id) {
+				todo.isComplete = !todo.isComplete;
+			}
+			return todo;
+		})
+		setTodos(updateTodos);
+	}
+
+	return (
+		<div>
+			<h1>Whats the plan for today</h1>
+			<TodoForm onSubmit={addTodo}/>
+			<Todo 
+				todos={todos}
+				completeTodo={completeTodo}
+				removeTodo={removeTodo}
+				updateTodo={updateTodo}
+			/>
+		</div>
+	)
+}
+
+export default TodoList;
